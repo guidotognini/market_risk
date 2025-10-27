0ead8400644176423a08aff1a63ff92fbda79661
guidotognini <guidotognini@gmail.com>
2025-10-27 19:38:18 +0000

    bronze table definition

--- Medallion Architecture/bronze/bronze_fx_daily_rates.sql
+++ Medallion Architecture/bronze/bronze_fx_daily_rates.sql
@@ -1,4 +1,21 @@
 CREATE OR REFRESH STREAMING TABLE bronze_fx_daily_rates
+(
+  CONSTRAINT valid_ticker EXPECT (ticker IS NOT NULL),
+  CONSTRAINT valid_results EXPECT (results IS NOT NULL)
+)
 AS
-SELECT *
-FROM cloud_files('/Volumes/tabular/dataexpert/guidotognini/market_risk/raw_fx_daily_rates/*', 'json')
+SELECT
+  ticker,
+  results,
+  _metadata.file_path AS source_file,
+  _metadata.file_modification_time AS ingestion_time
+FROM read_files(
+  '/Volumes/tabular/dataexpert/guidotognini/market_risk/raw_fx_daily_rates/*',
+  'json',
+  MAP(
+    'cloudFiles.includeExistingFiles', 'true',
+    'cloudFiles.schemaLocation', '/Volumes/tabular/dataexpert/guidotognini/market_risk/_schemas/fx_daily_rates_bronze',
+    'cloudFiles.inferColumnTypes', 'true',
+    'cloudFiles.schemaEvolutionMode', 'rescue'
+  )
+);
